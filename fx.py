7dfd12467a89a198c29fbca8b2710ca0c5805021
shivambhat <shivambhat.sb@gmail.com>
2020-06-25 11:26:55 -0700

    adding more files

---
+++ fx.py
@@ -0,0 +1,11 @@
+def is_leap(year):
+    leap = False
+    if (year % 4)== 0:
+        return True
+    else:
+
+        return leap
+    year = int(input())
+is_leap(year)
+year = int(input())
+print(is_leap(year))
